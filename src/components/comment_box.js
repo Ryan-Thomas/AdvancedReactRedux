76f1f059ffcf8313b0f2f88d73af45f4221d2eb2
Ryan Thomas <ryanthomas970@gmail.com>
2017-09-11 15:16:37 -0700

    Spec Failures after Code Change

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,39 +1,38 @@
 import * as React from 'react'
 import { connect } from 'react-redux'
-import * as actions from '../actions'
+import { saveComment } from '../actions/index'
 
 class CommentBox extends React.Component {
   constructor(props) {
     super(props)
     this.state = { comment: '' }
   }
-  
+
   handleChange = (event) => {
     this.setState({comment: event.target.value })
   }
-  
+
   handleSubmit = (event) => {
     event.preventDefault()
-    
+
     this.props.saveComment(this.state.comment)
     this.setState({ comment: '' })
   }
-  
+
   render() {
     return (
       <form onSubmit={this.handleSubmit} className="comment-box">
-        <textarea 
-          value={this.state.comment} 
-          onChange={this.handleChange} 
+        <h4>Add a comment</h4>
+        <textarea
+          value={this.state.comment}
+          onChange={this.handleChange}
         />
-        <button action="submit">Submit Comment!</button>
+        <div>
+          <button action="submit">Submit Comment!</button>
+        </div>
       </form>
     )
   }
 }
 
-const mapStateToProps = props => {
-  
-}
-
-export default connect(null, { actions })(CommentBox)
+export default connect(null, { saveComment })(CommentBox)
