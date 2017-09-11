4ad04fa8276ba3935920073165410e20d91c892b
Ryan Thomas <ryanthomas970@gmail.com>
2017-09-11 16:22:04 -0700

    Simulate Helper

--- test/test_helper.js
+++ test/test_helper.js
@@ -3,10 +3,11 @@ import jquery from 'jquery'
 import TestUtils from 'react-addons-test-utils'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { expect } from 'chai'
+import chai, { expect } from 'chai'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import reducers from '../src/reducers'
+import chaiJquery from 'chai-jquery'
 
 // Set up testing environment to run like a browser in the command line
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>')
@@ -25,6 +26,14 @@ const renderComponent = (ComponentClass, props, state) => {
 }
 
 // Build helper for simulating events
+$.fn.simulate = function(eventName, value) {
+  if (value) {
+    this.val(value)
+  }
+  TestUtils.Simulate[eventName](this[0])
+}
 
 // Set up chai-jquery
+chaiJquery(chai, chai.util, $)
+
 export { renderComponent, expect }
