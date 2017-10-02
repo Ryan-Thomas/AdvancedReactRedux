f44440736b8236bd6bcc95df4904885fcd10cade
Ryan Thomas <ryanthomas970@gmail.com>
2017-10-02 00:11:08 -0700

    The Field Component

--- router+form/src/components/posts_new.jsx
+++ router+form/src/components/posts_new.jsx
@@ -2,12 +2,26 @@ import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
 
 export class PostsNew extends Component {
+  // renderTitleField takes a parameter of field
+  // field contains some event handlers that we need to wire up to the JSX that we are returning
+  renderTitleField(field) {
+    return (
+      <div>
+        <input 
+          type="text"
+          // field.input is an object which contains a bunch of different event-handlers like onchange, onblur...
+          {...field.input}
+        />
+      </div>
+    )
+  }
+
   render() {
     return (
       <form>
         <Field
           name="title"
-          component={}
+          component={this.renderTitleField}
         />
       </form>
     )
