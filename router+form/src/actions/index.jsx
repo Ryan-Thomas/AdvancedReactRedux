98c65d08969726b9d7185e45f9625f9ed8a15459
Ryan Thomas <ryanthomas970@gmail.com>
2017-10-01 23:30:51 -0700

    Rendering a List of Posts

--- router+form/src/actions/index.jsx
+++ router+form/src/actions/index.jsx
@@ -6,6 +6,7 @@ const ROOT_URL = 'http://reduxblog.herokuapp.com/api'
 const API_KEY = '?key=ryanthomas'
 
 export function fetchPosts() {
+  console.log('ROOT_URL', ROOT_URL)
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`)
   return {
     type: FETCH_POSTS,
