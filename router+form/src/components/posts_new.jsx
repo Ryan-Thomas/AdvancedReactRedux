185ecce6a3b9791d9a9fb0d1352ead8648150da9
Ryan Thomas <ryanthomas970@gmail.com>
2017-10-02 00:23:49 -0700

    Generalizing Fields

--- router+form/src/components/posts_new.jsx
+++ router+form/src/components/posts_new.jsx
@@ -4,11 +4,13 @@ import { Field, reduxForm } from 'redux-form'
 export class PostsNew extends Component {
   // renderTitleField takes a parameter of field
   // field contains some event handlers that we need to wire up to the JSX that we are returning
-  renderTitleField(field) {
+  renderField(field) {
     return (
-      <div>
+      <div className="form-group">
+        <label>{field.label}</label>
         <input 
           type="text"
+          className="form-control"
           // field.input is an object which contains a bunch of different event-handlers like onchange, onblur...
           {...field.input}
         />
@@ -16,12 +18,24 @@ export class PostsNew extends Component {
     )
   }
 
+
   render() {
     return (
       <form>
         <Field
+          label="Title"
           name="title"
-          component={this.renderTitleField}
+          component={this.renderField}
+        />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
         />
       </form>
     )
